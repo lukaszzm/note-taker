7dd8dbe887e0fc70ece44dcd21437c2d34636469
LukaszzM <lukaszzmalucha@gmail.com>
2025-06-21 14:32:15 +0200

    remove login from proxy url

--- features/shared/lib/proxy.ts
+++ features/shared/lib/proxy.ts
@@ -1,10 +1,9 @@
 export function getProxyUrl(url: string | null): string {
   const baseURL = process.env.BASE_URL as string;
-  const login = process.env.LOGIN as string;
 
   if (!url) {
-    return `${baseURL}/${login}`;
+    return `${baseURL}`;
   }
 
-  return `${baseURL}/${login}/${url}`;
+  return `${baseURL}/${url}`;
 }
